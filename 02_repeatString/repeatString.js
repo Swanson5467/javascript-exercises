f38d82112fc328c403e7623877ba33851d1f5807
Alex Swanson <swanson5467@gmail.com>
2023-01-05 16:41:44 -0500

    Test and complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let resultString = '';
+    for (i = 0; i < num; i++) {
+        resultString += string;
+    }
+    return resultString;
 
 };
 
