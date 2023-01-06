06cae1afe8794ba3fd5f709cfa7e463de14ad575
Alex Swanson <swanson5467@gmail.com>
2023-01-05 23:13:08 -0500

    Test and complete exercise 06 - leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if ( (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0) ) { // (div by 400) OR (div by 4 AND not by 100)
+        return true;
+    } else {
+        return false;
+    }
 
 };
-
 // Do not edit below this line
 module.exports = leapYears;
