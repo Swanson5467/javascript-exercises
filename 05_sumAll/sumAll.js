40d17517b17ac387c4ff7e8b6e3ac688dc28d319
Alex Swanson <swanson5467@gmail.com>
2023-01-05 22:54:45 -0500

    Test and complete exercise 05 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0 || typeof(x) != 'number' || typeof(y) != 'number') {
+        return 'ERROR';
+    }
+    let sum = 0
+    let start = y;
+    let end = x;
+    if (y > x) {
+        start = x;
+        end = y;
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
+console.log(sumAll(1, '4'));
 // Do not edit below this line
 module.exports = sumAll;
