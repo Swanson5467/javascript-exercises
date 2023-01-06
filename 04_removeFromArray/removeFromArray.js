99f5f762369dcb292874cb38f450bad5a8d48309
Alex Swanson <swanson5467@gmail.com>
2023-01-05 22:23:32 -0500

    Test and complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (i=0; i<array.length; i++) {
+        let indexOfRemoved = array.indexOf(array[i]);
+        for (j=0; j<args.length;j++) {
+            if (args[j] === array[i]) {
+                array.splice(indexOfRemoved, 1);
+                i -= 1;
+                if (array.length == 0) {
+                    return array;
+                }
+            }
+        }
+    }
+    return array;
+}
 
-};
-
+removeFromArray(['hey',2,3,'ho'],'hey',3)
 // Do not edit below this line
 module.exports = removeFromArray;
