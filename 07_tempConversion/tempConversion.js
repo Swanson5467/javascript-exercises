d3dd3bfa035a3deece1278cd5f3d69a3d6d3b08f
Alex Swanson <swanson5467@gmail.com>
2023-01-05 23:52:08 -0500

    Test and complete exercise 07 - tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // subtract 32 and * 5/9
+  temp -= 32;
+  temp *= 5/9;
+  let roundedTemp = Math.round(temp*10)/10;
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // divide by 5/9 and add 32
+  temp *= 9/5;
+  temp += 32;
+  let roundedTemp = Math.round(temp*10)/10;
+  return roundedTemp;
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
