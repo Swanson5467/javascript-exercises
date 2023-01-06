f05c58159f31bb5bed37dc28c62f80bdf9f30119
Alex Swanson <swanson5467@gmail.com>
2023-01-06 00:12:46 -0500

    Test and complete exercise 08 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let i=0; i<array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (let i=0; i<array.length; i++) {
+    product *= array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }	else {
+    let fac = 1;
+    for (let i = num; i > 0; i--) {
+      fac *= i;
+    }
+    return fac;
+  }
 };
-
+console.log(factorial(5));
 // Do not edit below this line
 module.exports = {
   add,
