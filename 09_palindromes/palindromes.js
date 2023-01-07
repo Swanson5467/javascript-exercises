7a7659f50f5c4f1d1a36a25dd2c9d015871c456a
Alex Swanson <swanson5467@gmail.com>
2023-01-07 15:29:27 -0500

    Test and complete exercise 09 - palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let processed = string.replace(/[^A-Za-z]/g, "");
+    let split = processed.split("");
+    let reversed = split.reverse();
+    let opposite = reversed.join("");
+    if (processed.toUpperCase() === opposite.toUpperCase()) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;
