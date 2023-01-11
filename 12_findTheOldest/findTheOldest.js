3599aa80fe56222b83a4956d132ae26de8776fa6
Alex Swanson <swanson5467@gmail.com>
2023-01-11 16:26:12 -0500

    Test and complete exercise 12 - findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentYear = 2023;
+    let oldest = 0;
+    let oldestAge = 0;
+    for (const item of people) {
+        let age = item.yearOfDeath - item.yearOfBirth;
+        if ('yearOfDeath' in item === false) {
+            age = currentYear - item.yearOfBirth;
+        }
+        if (age > oldestAge) {
+            oldest = item;
+            oldestAge = age;
+        }
+    }
+    return oldest;
 
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
