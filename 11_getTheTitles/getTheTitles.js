88a32bcdd68878342b679da3878244b791d893c2
Alex Swanson <swanson5467@gmail.com>
2023-01-11 15:47:02 -0500

    Test and complete exercise 11 - getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let titles = [];
+    for (const item of books) {
+        titles.push(item.title);
+    }
+    return titles;
 
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
